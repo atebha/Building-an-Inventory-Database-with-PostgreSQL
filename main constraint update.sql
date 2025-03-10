da773db8d86de64fef81c36feb246854475d5037
atebha <ahsantebha@outlook.com>
2025-03-10 14:00:23 -0700

    formatting of comments for reorder table and refacotr of locations table

--- main constraint update.sql
+++ main constraint update.sql
@@ -27,6 +27,7 @@ WHERE manufacturer_id IN (1, 2);
 INSERT INTO parts (id, description, code, manufacturer_id) 
 VALUES (54, '', 'V1-009', 9);
 
+-- Set not nulls, add in checks and a foriegn key to the reorder_options table
 -- step 6
 ALTER TABLE reorder_options
 ALTER COLUMN price_usd SET NOT NULL,
@@ -35,19 +36,13 @@ ADD CHECK (price_usd > 0 AND quantity > 0),
 ADD CHECK (price_usd/quantity > 0.02 AND price_usd/quantity < 25.00),
 ADD FOREIGN KEY (part_id) REFERENCES parts (id);
 
--- step 10 
+-- Add in a check for qty, unique value and foriegn key to the locations table
+-- step 7 
 ALTER TABLE locations 
-ADD CHECK (qty > 0); 
-
--- step 11
-ALTER TABLE locations
-ADD UNIQUE (part_id, location);
-
--- step 12
-ALTER TABLE locations
+ADD CHECK (qty > 0),
+ADD UNIQUE (part_id, location),
 ADD FOREIGN KEY (part_id) REFERENCES parts (id);
 
--- step 13
 
 -- step 14
 INSERT INTO manufacturers(name, id) 
